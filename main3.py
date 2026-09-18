7c7e7c5abe4eae49800fbe82ca6bbbb6e8e66158
Prih Roshni <prihroshniroshni@gmail.com>
2026-09-18 15:44:32 +0500

    main3.py

--- main3.py
+++ main3.py
@@ -1,8 +1,15 @@
 #multiplication of 2 
-
-
 i = 1
 
 while i <= 10:          
     print(2* i)
     i += 1
+
+
+#number will be given by user 
+i = 1
+n = int(input("input a number:  " ))
+
+while i <= 10:          
+    print(n * i)
+    i += 1 
