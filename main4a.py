7a38c35a4c2bc196346c3a7cd2e2e5809b76faa5
Prih Roshni <prihroshniroshni@gmail.com>
2026-09-18 17:00:30 +0500

    main4a.py

---
+++ main4a.py
@@ -0,0 +1,8 @@
+#print the name is list 
+list = ["ironman", "thor" , "batman" , "spiderman" , "hero" , "villain" ]
+
+ind = 0 
+
+while ind < len(list):          
+    print(list[ind])
+    ind += 1
